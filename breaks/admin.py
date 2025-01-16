cbb1834e3c941b1df023873e40b01c375b2388b6
Damir <nimadjonov.2404@gmail.com>
2025-01-16 14:11:52 +0500

    done: get some knowleges about admin ponel

--- breaks/admin.py
+++ breaks/admin.py
@@ -3,6 +3,7 @@ from django.contrib.admin import TabularInline
 
 from breaks.models import organizations, groups, replacements, dicts, breaks
 
+
 ######################
 # IN LINES
 ######################
@@ -19,17 +20,23 @@ class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'director')
     list_display_links = ('id', 'name')
 
+    filter_horizontal = ('employee',)
+
 
 @admin.register(groups.Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'manager', 'min_active')
     list_display_links = ('id', 'name')
 
+    search_fields = ('name',)
+
 
 @admin.register(replacements.Replacement)
 class ReplacementAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'group', 'break_start')
     list_display_links = ('id', 'date', 'group')
+
+    autocomplete_fields =('group',)
 
     inlines = (
         ReplacementEmployeeInline,
@@ -47,7 +54,9 @@ class BreakStatusAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'sort', 'is_active')
     list_display_links = ('name', 'code')
 
+
 @admin.register(breaks.Break)
 class BreakAdmin(admin.ModelAdmin):
     list_display = ('id', 'replacement', 'break_start', 'break_end',)
     list_display_links = ('id', 'replacement',)
+    list_filter = ('status',)
--- breaks/models/organizations.py
+++ breaks/models/organizations.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Organization(models.Model):
     name = models.CharField(max_length=225)
     director = models.ForeignKey(User, models.RESTRICT, 'organization_directors')
-    employees = models.ManyToManyField(User, 'organization_employees')
+    employee = models.ManyToManyField(User, 'organization_employees')
 
     def __str__(self):
         return f'{self.name} - {self.pk}'
